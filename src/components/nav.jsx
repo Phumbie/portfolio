22d07aa9463207838c4d4c105d828ffda4f7b259
phumbie <fumbie40@gmail.com>
2020-06-29 22:50:05 +0100

    added short note

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React from 'react';
 
 const Nav = () => {
-  const expand = {
-    height: "100%",
-    width: "100%",
-  };
+	const expand = {
+		height: '100%',
+		width: '100%'
+	};
 
-  return (
-    <div className="nav">
-      {/* <div className="logo">
+	return (
+		<div className="nav">
+			<marquee behavior="scroll" direction="left">
+				work still in progress
+			</marquee>
+
+			{/* <div className="logo">
 				<img style={expand} src="/images/logo-name.svg" alt="logo" />
 			</div> */}
-      <div className="menu">
-        <img style={expand} src="/images/menu.svg" alt="menu" />
-      </div>
-    </div>
-  );
+			<div className="menu">
+				<img style={expand} src="/images/menu.svg" alt="menu" />
+			</div>
+		</div>
+	);
 };
 export default Nav;
